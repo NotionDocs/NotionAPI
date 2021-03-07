b9a3a0746499b13c579a873e846deb93904d0ede
Cole Gawin <colegawin@gmail.com>
2021-03-06 20:33:43 -0600

    🔧(core): use named export for createNotionService

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,7 +2,7 @@ import { NotionClient } from "@notionapi/client";
 import { NotionCollectionService } from "./collection";
 import { NotionPageService } from "./page";
 
-const createNotionService = (client: NotionClient) => {
+export const createNotionService = (client: NotionClient) => {
   const page = new NotionPageService(client);
   const collection = new NotionCollectionService(client, page);
   return { page, collection };
@@ -11,4 +11,3 @@ const createNotionService = (client: NotionClient) => {
 export * from "./collection";
 export * from "./page";
 export * from "./util";
-export default createNotionService;
