3b645e641d6e8d0abb25da8e6db3d66f5277cc1d
Cole Gawin <colegawin@gmail.com>
2021-03-06 21:15:59 -0600

    ✏️(client): remove unused methods

--- packages/client/src/NotionClient.ts
+++ packages/client/src/NotionClient.ts
@@ -1,6 +1,4 @@
 import { HTTPService } from "./HTTPService";
-import { BaseCollectionView, CollectionInstance, PageChunk } from "notion-types";
-import { dashifyId } from "./dashifyId";
 
 const NOTION_API_URL = "https://www.notion.so/api/v3/";
 
@@ -14,42 +12,5 @@ export class NotionClient {
         data,
       })
     ).responseData;
-  }
-
-  public async queryPage(req: { pageId: string }) {
-    return (
-      await this.httpService(NOTION_API_URL + "loadPageChunk", {
-        token_v2: this.token,
-        data: {
-          pageId: dashifyId(req.pageId),
-          cursor: { stack: [] },
-          chunkNumber: 0,
-          limit: 100,
-          verticalColumns: false,
-        },
-      })
-    ).responseData as PageChunk;
-  }
-
-  public async queryCollection(req: {
-    collectionId: string;
-    collectionViewId: string;
-    query?: BaseCollectionView["query2"];
-  }) {
-    return (
-      await this.httpService(NOTION_API_URL + "queryCollection", {
-        token_v2: this.token,
-        data: {
-          collectionId: dashifyId(req.collectionId),
-          collectionViewId: dashifyId(req.collectionViewId),
-          loader: {
-            type: "table",
-            limit: Number.MAX_SAFE_INTEGER,
-            loadContentCover: true,
-          },
-          query: req.query,
-        },
-      })
-    ).responseData as CollectionInstance;
   }
 }
