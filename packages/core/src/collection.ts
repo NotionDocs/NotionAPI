492821f0a22857527edb32014fac611e6bb015f6
Cole Gawin <colegawin@gmail.com>
2021-03-06 18:42:46 -0600

    🚀(core): add fetch collection method

--- packages/core/src/collection.ts
+++ packages/core/src/collection.ts
@@ -1,4 +1,4 @@
-import { CollectionInstance } from "notion-types";
+import { CollectionInstance, PageChunk } from "notion-types";
 import { CollectionPage } from "./util";
 import { NotionClient } from "@notionapi/client";
 import { NotionPageService } from "./page";
@@ -22,4 +22,30 @@ export class NotionCollectionService {
 
   extractMetadataFrom = (collection: CollectionInstance) =>
     this.pageService.extractMetadataFrom(collection.recordMap, Object.keys(collection.recordMap.block)[0]);
+
+  async fetch(pageId: string, collectionViewId: string, limit: number = 50) {
+    const page = (await this.client.query("loadPageChunk", {
+      pageId,
+      cursor: { stack: [] },
+      chunkNumber: 0,
+      limit: 100,
+      verticalColumns: false,
+    })) as PageChunk;
+
+    const collectionId = Object.keys(page.recordMap.collection)[0];
+    const query = page.recordMap.collection_view[collectionViewId].value.query2;
+
+    return (await this.client.query("queryCollection", {
+      collectionId,
+      collectionViewId,
+      loader: {
+        limit,
+        loadContentCover: true,
+        searchQuery: "",
+        type: "table",
+        userTimeZone: "America/Chicago",
+      },
+      query,
+    })) as CollectionInstance;
+  }
 }
