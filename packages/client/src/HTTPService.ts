b7148988034609bbb46f1ff9da8040664bdcc377
Cole Gawin <colegawin@gmail.com>
2021-04-25 00:02:50 -0500

    🚀: add docs comment for HTTPService

--- packages/client/src/HTTPService.ts
+++ packages/client/src/HTTPService.ts
@@ -1,3 +1,6 @@
+/**
+ * HTTP Service interface used by NotionAPI client. To be implemented by user. Accepts url to send request to and request information. Returns a Promise with ret
+ */
 export type HTTPService = (
   url: string,
   request: {
